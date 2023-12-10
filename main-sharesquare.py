970675c5071b2eee912f5c868f788ada5b556431
VulcanRaven <patilabhay1357@gmail.com>
2023-12-10 17:45:57 +0530

    split working

--- main-sharesquare.py
+++ main-sharesquare.py
@@ -2,10 +2,12 @@ import psycopg2
 import tkinter
 from tkinter import messagebox
 from tkinter.ttk import *
+from tkinter.scrolledtext import ScrolledText
 from PIL import ImageTk, Image
 
 #DB Conn for login page
 def loginpage():
+    global active_user
     conn = psycopg2.connect(
         host="localhost",
         database="sharesquare1",
@@ -26,11 +28,13 @@ def loginpage():
         usernamer = username_result
 
         if email_Entry.get()==username_result and pwd_Entry.get()==password_result:
+            active_user = email_Entry.get()
             mainwindow()        
         else:
             messagebox.showinfo(title="Login",message="Incorrect Email or password")
     else:
         messagebox.showinfo(title="Invalid Input",message="Please fill in all the fields!")
+    conn.close()
 
 #DB Conn for register page
 def regpage():
@@ -60,15 +64,29 @@ def regpage():
 
     else:
         messagebox.showinfo(title="Invalid Input",message="Please fill in all the fields!")   
-        swin.focus()         
+        swin.focus()        
+    conn.close() 
 
-#Calling signup DB Command
-def register():
-    regpage()
+#DB conn for groups page
+def submit():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="sharesquare1",
+        user="postgres",
+        password="welcome1234")
+    cur = conn.cursor()
 
-#Calling login DB Command
-def login():
-    loginpage()
+    insert_users = (f"INSERT into public.groups1 (groupname, leader, members) VALUES ('{Entry_groupname.get()}','{active_user}','{Entry_members.get()}');")
+
+    if len(Entry_groupname.get())!=0 and len(Entry_members.get())!=0:
+        cur.execute(insert_users)
+        conn.commit()
+        messagebox.showinfo(title='Success!',message="Group has been created!")
+        window1.focus()
+    else:
+        messagebox.showinfo(title="Inavlid Input",message="Please fill in all the fields!")
+        window1.focus()
+    conn.close()
 
 #colors
 bg1='#1b1b1b'
@@ -95,13 +113,180 @@ email_label=tkinter.Label(frame,text="Username",bg=bg1,fg="White")
 email_Entry=tkinter.Entry(frame)
 pwd_label=tkinter.Label(frame,text="Password",bg=bg1,fg="White")
 pwd_Entry=tkinter.Entry(frame,show="*")
-login_button=tkinter.Button(frame,text="Login",bg=c2,fg="White",command=login)
+login_button=tkinter.Button(frame,text="Login",bg=c2,fg="White",command=loginpage)
 login_label.grid(row=0,column=0,columnspan=2,pady=40)
 email_label.grid(row=1,column=0)
 email_Entry.grid(row=1,column=1)
 pwd_label.grid(row=2,column=0)
 pwd_Entry.grid(row=2,column=1)
 login_button.grid(row=3,column=0,columnspan=2,pady=20)
+
+#split bill page
+#next
+def next():
+    #DB conn for profile page
+    global profile_details
+    conn = psycopg2.connect(
+        host="localhost",
+        database="sharesquare1",
+        user="postgres",
+        password="welcome1234")
+    cur = conn.cursor()
+
+    check_groups = (f"Select groupname, members from public.groups1 where leader = ('{active_user}');")
+    cur.execute(check_groups)
+    result2=cur.fetchall()
+    options=[]
+    for i in range(len(result2)):
+        options.append(result2[i][0])
+    print(options)  
+
+    def input():
+        conn = psycopg2.connect(
+        host="localhost",
+        database="sharesquare1",
+        user="postgres",
+        password="welcome1234")
+        cur = conn.cursor()
+
+        check_members= (f"select members from public.groups1 where groupname = ('{menu.get()}'); ")
+        cur.execute(check_members)
+        result3=cur.fetchall()
+        x=result3[0][0].split(",")
+        d=""
+        for i in range(len(x)):
+            d+=str(i+1)+") "+x[i]
+            d+="\n"+"\n"           
+        
+        frame5.destroy()
+        frame6=tkinter.Frame(window4,bg=bg1)
+        frame6.pack()
+        label6=tkinter.Label(frame6,text="Members",bg=bg1,fg="White",font=("Arial 16 bold",16))
+        label6.grid(row=0, column=0, columnspan=2, padx=(170,160), pady=(40,0))
+        label_profile6=tkinter.Text(frame6,bg=bg1,fg="White",height=5, width=30, font="Arial 16")
+        v=Scrollbar(frame6, orient='vertical')
+        v.grid(column=7, pady=(0,0),padx=(10,0))
+        v.config(command=label_profile6.yview)    
+        label_profile6.insert(tkinter.END, d)
+        label_profile6.configure(state='disabled', yscrollcommand=v.set)
+        label_profile6.grid(row=1,column=1,columnspan=6,pady=(10,0),padx=(40,90))
+        label7=tkinter.Label(frame6,text="Bill Amount",bg=bg1,fg="White",font=("Arial 16 bold",16))
+        label7.grid(row=3, column=0, columnspan=2, padx=(170,160), pady=(100,0))
+        Entry_members=tkinter.Entry(frame6)
+        Entry_members.grid(row=3,column=0,columnspan=2, padx=(170,160), pady=(180,0))
+        conn.close()
+        
+    window4 = tkinter.Toplevel(root)
+    window4.title("ShareSquare - Split Bill")
+    window4['background']=bg1
+    window4.geometry('%dx%d+%d+%d' % (width, height, x, y))
+    window4.resizable(0,0)
+
+    frame5=tkinter.Frame(window4,bg=bg1)
+    frame5.pack()
+
+    menu= tkinter.StringVar()    
+    #Create a dropdown Menu
+    drop= OptionMenu(frame5, menu, *options)
+    drop.grid(row=0,column=0,columnspan=2, padx=(0,0), pady=(80,0))    
+    button1 = tkinter.Button(frame5,text = "Enter",bg=c2,fg="White", command=input)
+    button1.grid(row=1,column=0,padx=(20,0), pady=(20,0)) 
+    label5=tkinter.Label(frame5,text="Choose Group",bg=bg1,fg="White",font=("Arial",16))
+    label5.grid(row=0, column=0, columnspan=2, pady=(0,0))
+    conn.close()
+    window1.destroy()   
+
+
+def splitbill():
+    global Entry_groupname, Entry_members, window1
+
+    window1 = tkinter.Toplevel(root)
+    window1.title("ShareSquare - Split Bill")
+    window1['background']=bg1
+    window1.geometry('%dx%d+%d+%d' % (width, height, x, y))
+    window1.resizable(0,0)
+    
+    frame4=tkinter.Frame(window1,bg=bg1)
+    frame4.pack()
+    label2=tkinter.Label(frame4,text="Create Group",bg=bg1,fg="White",font=("Arial",16))    
+    label=tkinter.Label(frame4,text="Group Name",bg=bg1,fg="White")
+    label1=tkinter.Label(frame4,text="Members",bg=bg1,fg="White")
+    Entry_members=tkinter.Entry(frame4)
+    Entry_groupname=tkinter.Entry(frame4)
+    label2.grid(row=0,column=0,columnspan=2,pady=40)
+    label.grid(row=1,column=0)
+    Entry_groupname.grid(row=1,column=1)
+    label1.grid(row=2,column=0)
+    Entry_members.grid(row=2,column=1)
+    submit_button=tkinter.Button(frame4,text="Submit",bg=c2,fg="White",command=submit)
+    submit_button.grid(row=3,column=0,columnspan=2,pady=20)
+    next_button=tkinter.Button(frame4,text="Next",bg=c2,fg="White",command=next)
+    next_button.grid(row=5,column=0,columnspan=2,pady=20)
+
+#your debts page
+def your_debts():
+    window2 = tkinter.Toplevel(root)
+    window2.title("ShareSquare - Debts")
+    window2['background']=bg1
+    window2.geometry('%dx%d+%d+%d' % (width, height, x, y))
+    window2.resizable(0,0)
+
+#user profile page 
+def profile():
+    #DB conn for profile page
+    global profile_details
+    conn = psycopg2.connect(
+        host="localhost",
+        database="sharesquare1",
+        user="postgres",
+        password="welcome1234")
+    cur = conn.cursor()
+
+    check_users = (f"SELECT username, name, email from public.loginpage where username = ('{active_user}');")
+    check_groups = (f"Select groupname, members from public.groups1 where leader = ('{active_user}');")
+    cur.execute(check_users)
+    result=cur.fetchone()
+    profile_uname=(result[0])
+    profile_name=(result[1])
+    profile_email=(result[2])  
+    cur.execute(check_groups)
+
+    result1=cur.fetchall()
+    d=""
+    for i in range(len(result1)):
+        d+=str(i+1)+") "+result1[i][0]+" - "+result1[i][1]
+        d+="\n"+"\n"
+    print(d)  
+
+    window3 = tkinter.Toplevel(root)
+    window3.title("ShareSquare - Your Profile")
+    window3['background']=bg1
+    window3.geometry('%dx%d+%d+%d' % (width, height, x, y))
+    window3.resizable(0,0)
+
+    label_profile=tkinter.Label(window3,text="Username:",bg=bg1,fg="White", font="Arial 16 bold")
+    label_profile.grid(row=0,column=0,columnspan=2,pady=(40,0),padx=(40,0))
+    label_profile1=tkinter.Label(window3,text="Name:",bg=bg1,fg="White", font="Arial 16 bold")
+    label_profile1.grid(row=2,column=0,columnspan=2)
+    label_profile2=tkinter.Label(window3,text="E-mail:",bg=bg1,fg="White", font="Arial 16 bold")
+    label_profile2.grid(row=4,column=0,columnspan=2)
+    label_profile3=tkinter.Label(window3,text=profile_uname,bg=bg1,fg="White", font="Arial 16")
+    label_profile3.grid(row=0,column=3,columnspan=2,pady=(40,0),padx=(40,0))
+    label_profile4=tkinter.Label(window3,text=profile_name,bg=bg1,fg="White", font="Arial 16")
+    label_profile4.grid(row=2,column=3,columnspan=2)
+    label_profile4=tkinter.Label(window3,text=profile_email,bg=bg1,fg="White", font="Arial 16")
+    label_profile4.grid(row=4,column=3,columnspan=2)
+    label_profile5=tkinter.Label(window3,text="Groups:",bg=bg1,fg="White", font="Arial 16 bold")
+    label_profile5.grid(row=8,column=0,columnspan=2,padx=(15,0))
+        
+    label_profile6=tkinter.Text(window3,bg=bg1,fg="White",height=5, width=30, font="Arial 16 bold")
+    v=Scrollbar(window3, orient='vertical')
+    v.grid(sticky='e',column=7, pady=(10,0))
+    v.config(command=label_profile6.yview)    
+    label_profile6.insert(tkinter.END, d)
+    label_profile6.configure(state='disabled', yscrollcommand=v.set)
+    label_profile6.grid(row=9,column=1,columnspan=6,pady=(40,0),padx=(40,90)) 
+    conn.close()
 
 #main window
 def mainwindow():
@@ -118,16 +303,19 @@ def mainwindow():
     img = ImageTk.PhotoImage(Image.open(r"resources\Image.png"))
     cv.create_image(15, 22.5,anchor=tkinter.NW, image=img)
     cv.create_text(130,40,text="ShareSquare",fill="White",font="Arial 16 bold")    
-    cv.create_text(209,65,text="Friendly way to split bills with your friends!",fill="White",font="Arial 12")
-    
+    cv.create_text(209,65,text="Friendly way to split bills with your friends!",fill="White",font="Arial 12") 
 
     #buttons
     frame3=tkinter.Frame(window,bg=bg1)
     frame3.pack()
-    split_bill=tkinter.Button(frame3,text="Split Bill",bg=c2,fg="White")
-    split_bill.grid(row=37,column=0,columnspan=2)
-    window.mainloop()
-    
+    split_bill=tkinter.Button(frame3,text="Split your Bill",bg=c2,fg="White",command=splitbill)
+    split_bill.grid(row=0,column=0, pady=70)#, padx=(240,0))
+    debt_check=tkinter.Button(frame3,text="Your Debts",bg=c2,fg="White",command=your_debts)
+    debt_check.grid(row=1,column=0)#, padx=(240,0))
+    user_profile=tkinter.Button(frame3,text="Your Profile",bg=c2,fg="White",command=profile)
+    user_profile.grid(row=2,column=0,pady=70)#, padx=(240,0))
+    window.mainloop()   
+
 
 #signup window
 def signup():
@@ -165,7 +353,7 @@ def signup():
     cpwd_label.grid(row=4,column=0)
     cpwd_Entry.grid(row=4,column=1)
 
-    cacc=tkinter.Button(frame2,text="Sign Up",bg=c2,fg="White", command=register)
+    cacc=tkinter.Button(frame2,text="Sign Up",bg=c2,fg="White", command=regpage)
     cacc.grid(row=5,column=0,columnspan=2,pady=20)
 
 signup_button=tkinter.Button(frame,text="Sign Up",bg=c2,fg="White",command=signup)
